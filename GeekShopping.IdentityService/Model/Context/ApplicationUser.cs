012fd2dd08051927d214df704a9c74e656592573
Adam <adam.machado12@gmail.com>
2023-07-09 14:48:05 -0300

    ADD: Identity with Duede

---
+++ GeekShopping.IdentityService/Model/Context/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace GeekShopping.IdentityService.Model.Context
+{
+    public class ApplicationUser : IdentityUser
+
+    {
+        public string FirtName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ GeekShopping.IdentityService/Startup.cs
@@ -0,0 +1,77 @@
+﻿
+using GeekShopping.IdentityService.Configuaration;
+using GeekShopping.IdentityService.Model.Context;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+
+
+
+namespace GeekShoopping.IdentityService
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var connection = Configuration["MySQlConnection:MySQlConnectionString"];
+
+            services.AddDbContext<MySQLContext>(options => options.
+                UseMySql(connection,
+                        new MySqlServerVersion(
+                            new Version(8, 0, 33, 0))));
+
+            services.AddIdentity<ApplicationUser,IdentityRole>()
+                .AddEntityFrameworkStores<MySQLContext>()
+                .AddDefaultTokenProviders();
+
+            var builder = services.AddIdentityServer(options =>
+
+            {
+                options.Events.RaiseErrorEvents = true;
+                options.Events.RaiseFailureEvents = true;
+                options.Events.RaiseInformationEvents = true;
+                options.Events.RaiseSuccessEvents = true;
+                options.EmitStaticAudienceClaim = true;
+
+            }).AddInMemoryIdentityResources(
+                IdentityConfiguration.IdentityResources)
+                    .AddInMemoryClients(IdentityConfiguration.Clients)
+                    .AddInMemoryApiScopes(IdentityConfiguration.ApiScopes)
+                    .AddAspNetIdentity<ApplicationUser>();
+            
+            builder.AddDeveloperSigningCredential();
+
+
+            services.AddControllersWithViews();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+
+            }
+
+            app.UseRouting();
+            app.UseIdentityServer();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
